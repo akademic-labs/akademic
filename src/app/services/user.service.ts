ff3af591e819fc6bacb445c0bb290e0ac15fb845
Patrick Lima <patrickgoncalveslima@hotmail.com>
2018-10-01 01:41:07 -0300

    [signup] new user now has the actual displayname

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestoreDocument, AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 
-import { User, Roles } from 'app/models/user.interface';
+import { User } from 'app/models/user.interface';
 
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { UserInfo } from 'firebase';
 import { Router } from '@angular/router';
 import { NotifyService } from './notify.service';
 
@@ -40,19 +39,10 @@ export class UserService {
     return this.getUser(id).update(data);
   }
 
-  public createUserData(authUser: UserInfo, roles: Roles) {
+  public createUserData(user: User) {
     // sets user data to firestore on login
-    const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${authUser.uid}`);
-    const data: User = {
-      uid: authUser.uid,
-      email: authUser.email,
-      displayName: authUser.displayName || 'nameless',
-      photoURL: authUser.photoURL,
-      status: 'A',
-      roles: roles,
-      createdAt: new Date()
-    };
-    userRef.set(data, { merge: true });
+    const userRef: AngularFirestoreDocument<User> = this.afs.doc(`users/${user.uid}`);
+    userRef.set(user, { merge: true });
 
     this._router.navigate(['/dashboard']);
     this._notify.update('success', 'Bem vindo!');
