fcb9641b4d037e85fbb56788af3c769eddc7b2dd
Luiz Henrique Correia <luizhenriquecorreia1997@gmail.com>
2018-10-17 13:06:49 -0300

    [pipe] implemented to-select-id + added dropdown institution to course

--- src/app/pages/admin/course/course.component.ts
+++ src/app/pages/admin/course/course.component.ts
@@ -1,38 +1,52 @@
+import { InstitutionService } from 'app/services/institution.service';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Course } from '../../../models/course.interface';
 import { CourseService } from '../../../services/course.service';
+import { Institution } from 'app/models/institution.interface';
 
-@Component({
+@Component({  
   selector: 'aka-course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit {
 
-  title = 'Cursos';
   button = 'Adicionar';
   courseForm: FormGroup;
   course: Course;
   courses$: Observable<Course[]>;
+  institutions$: Observable<Institution[]>;
   @ViewChild('inputFocus') focusIn: ElementRef;
+
+  opa = {
+    'uid': 'YBu8UKbebVMKlwK7Ete9',
+    'name': 'Facear'
+  };
 
   constructor(
     private _courseFormBuilder: FormBuilder,
-    private _courseService: CourseService
+    private _courseService: CourseService,
+    private _institutionService: InstitutionService
   ) { }
 
   ngOnInit() {
     this.courses$ = this._courseService.get();
+    this.institutions$ = this._institutionService.get();
     this.buildForm();
     this.focusIn.nativeElement.focus();
+  }
+
+  onSelectInstituition(value: Institution) {
+    this.courseForm.patchValue({ instituitionUid: value.uid });
   }
 
   buildForm() {
     this.courseForm = this._courseFormBuilder.group({
       uid: new FormControl({ value: null, disabled: true }),
-      description: [null, Validators.compose([Validators.required])]
+      description: [null, Validators.compose([Validators.required])],
+      instituitionUid: []
     });
   }
 
--- src/app/pipes/to-select-id.pipe.ts
+++ src/app/pipes/to-select-id.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ToSelectIdPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return null;
+    return value ? value.uid : null;
   }
 
 }
