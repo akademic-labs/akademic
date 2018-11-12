0e59dfb6d4fe9c01efd7dfb3745565f1f35cc11a
Patrick Lima <patrickgoncalveslima@hotmail.com>
2018-11-11 23:39:52 -0200

    performance and acessibility fixes

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { SharedModule } from './../../shared/shared.module';
+import { AdminRoutingModule } from './admin-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 import { InstitutionsComponent } from './institutions/institutions.component';
 
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    AdminRoutingModule
   ],
   declarations: [
     InstitutionsComponent,
