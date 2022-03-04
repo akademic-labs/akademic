a1b41a840210077072d47aa3250d3d219f16e443
Patrick Lima <patrickgdlima@gmail.com>
2022-03-03 21:44:49 -0300

    feat(tests): from karma to jest

---
+++ apps/atividades-complementares/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'atividades-complementares',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/atividades-complementares',
+};
