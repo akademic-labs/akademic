4a6c9c7ecaec27183a1ac14463f89aadbdb4ab69
Patrick Lima <patrick.lima@picpay.com>
2022-03-10 11:42:58 -0300

    test: jest config with angular

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,24 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  collectCoverage: true,
+  coverageReporters: ['json', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+  collectCoverageFrom: [
+    '**/*.(route|state|validator|component|service|store|guard|interceptor|interface|pipe|directive|helper|resolver).(js|ts)',
+    '!**/*.(wip|mock|config).(js|ts|tsx)',
+    '**/*.tsx',
+    '!**/_app.tsx',
+  ],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+};
