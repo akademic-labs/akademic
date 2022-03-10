2f7a62d082af1968958c92edad4aa933df097de7
Patrick Lima <patrick.lima@picpay.com>
2022-03-10 11:48:15 -0300

    chore: angular specific folder

---
+++ apps/angular/atividades-complementares/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  displayName: 'atividades-complementares',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  coverageDirectory: '../../../coverage/apps/angular/atividades-complementares',
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
